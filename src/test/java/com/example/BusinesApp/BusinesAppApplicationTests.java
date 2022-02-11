253eef661f49380fd053737a005b8a7bbdf751ca
MaciexDrop <macwla1994@gmail.com>
2022-02-11 18:36:00 +0100

    Initial commit

---
+++ src/test/java/com/example/BusinesApp/BusinesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusinesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
